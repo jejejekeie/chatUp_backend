2f0343172113fafc8db66aad3bfed038f4fcb1c0
jejejekeie <evap040@gmail.com>
2024-04-08 18:31:14 +0200

    Fix firebase key expo

--- src/main/java/com/chatup/backend/config/SwaggerConfig.java
+++ src/main/java/com/chatup/backend/config/SwaggerConfig.java
@@ -1,16 +1,34 @@
 package com.chatup.backend.config;
-
-import org.springdoc.core.models.GroupedOpenApi;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
     @Bean
     public GroupedOpenApi publicApi() {
         return GroupedOpenApi.builder()
-                .group("public-api")
+                .group("chatup-api")
                 .packagesToScan("com.chatup.backend.controllers")
+                .pathsToMatch("/public/**")
                 .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("ChatUp API").version("v1").description("Documentación API ChatUp"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .name("bearerAuth")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
     }
 }
