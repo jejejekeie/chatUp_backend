f7f933efa2c6cdf4d93a12d11324ae7ba33b87ee
jejejekeie <evap040@gmail.com>
2024-04-11 22:35:18 +0200

    Fixing warnings on user

--- src/main/java/com/chatup/backend/repositories/UserRepository.java
+++ src/main/java/com/chatup/backend/repositories/UserRepository.java
@@ -2,7 +2,6 @@ package com.chatup.backend.repositories;
 
 import com.chatup.backend.models.User;
 import com.mongodb.lang.NonNull;
-import com.mongodb.lang.Nullable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -13,7 +12,6 @@ import java.util.Set;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
-    @Nullable
     Optional<User> findByEmail(String email);
     @NonNull
     @Override
