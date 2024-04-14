42f9757a62a7aa4e8948c0e85d400b0b5f0b7503
jejejekeie <evap040@gmail.com>
2024-04-14 09:48:41 +0200

    Delete loadImage

--- src/main/java/com/chatup/backend/services/ImageService.java
+++ src/main/java/com/chatup/backend/services/ImageService.java
@@ -1,43 +1,24 @@
 package com.chatup.backend.services;
 
-import com.mongodb.client.gridfs.model.GridFSFile;
-import jakarta.annotation.Resource;
 import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.gridfs.GridFsOperations;
 
-import org.springframework.data.mongodb.gridfs.GridFsResource;
 import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 @Service
 public class ImageService {
 
     private final GridFsTemplate gridFsTemplate;
-    private final GridFsOperations operations;
 
-    public ImageService(GridFsTemplate gridFsTemplate, GridFsOperations operations) {
+    public ImageService(GridFsTemplate gridFsTemplate) {
         this.gridFsTemplate = gridFsTemplate;
-        this.operations = operations;
     }
 
     public String storeImage(MultipartFile file) throws IOException {
         ObjectId fileId = gridFsTemplate.store(file.getInputStream(), file.getOriginalFilename(), file.getContentType());
         return fileId.toString();
-    }
-
-    public Resource loadImage(String id) throws IOException {
-        GridFSFile gridFsFile = gridFsTemplate.findOne(new Query(Criteria.where("_id").is(id)));
-        if (gridFsFile == null) {
-            throw new FileNotFoundException("File not found with id " + id);
-        }
-
-        GridFsResource resource = operations.getResource(gridFsFile);
-        return (Resource) resource;
     }
 }
